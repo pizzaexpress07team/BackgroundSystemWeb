ce7abb6bad208d5be03c5c9464f8ba95ba7083cd
Knight Zeng <k642713867@live.com>
2019-05-04 00:03:44 +0800

    [rc] 修改api请求地址

--- src/api/utils.js
+++ src/api/utils.js
@@ -24,7 +24,7 @@ axios.interceptors.response.use(data=> {
   return Promise.resolve(err);
 });
 
-let base = 'http://3.86.76.105:8080';
+let base = 'http://54.152.9.39:8080';
 
 export const postRequest = (url, params) => {
   return axios({
